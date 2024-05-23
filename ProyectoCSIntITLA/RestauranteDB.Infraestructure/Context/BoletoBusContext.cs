ad960b7cbeec8408ecf3f9f30c7586c61db29fd5
Steven Nouel <89994716+snouelhub@users.noreply.github.com>
2024-05-22 20:30:18 -0400

    Implementando dbcontext, dbset y patron repositorio

---
+++ ProyectoCSIntITLA/RestauranteDB.Infraestructure/Context/BoletoBusContext.cs
@@ -0,0 +1,22 @@
+﻿using DBBoletoBus.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+
+namespace DBBoletoBus.Infraestructure.Context
+{
+    public class BoletoBusContext : DbContext
+    {
+        public BoletoBusContext(DbContextOptions<BoletoBusContext> dbContext) : base(dbContext) { 
+        
+           
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Asiento>().ToTable("", "");
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
---
+++ ProyectoCSIntITLA/RestauranteDB.Infraestructure/Repository/AsientoRepository.cs
@@ -0,0 +1,42 @@
+﻿using DBBoletoBus.Domain.Entities;
+using DBBoletoBus.Domain.Interfaces;
+using DBBoletoBus.Infraestructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace DBBoletoBus.Infraestructure.Repository
+{
+    public class AsientoRepository : IAsiento
+    {
+        private readonly BoletoBusContext DBBoletoBus;
+
+        public AsientoRepository(BoletoBusContext boletoBus)
+        {
+             
+        }
+        public Task<Asiento> Get(int idAsiento)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public Task<List<Asiento>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Save(Asiento asiento)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Save(List<Asiento> asientos)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(Asiento asiento)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
